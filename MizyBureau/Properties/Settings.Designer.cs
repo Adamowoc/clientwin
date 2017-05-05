0bffef19bf1b7e8b5be09a86ca6e3a6731615fd7
nimjaneb <bencurlier@hotmail.com>
2017-05-05 17:59:36 +0200

    revu de la DB

--- MizyBureau/Properties/Settings.Designer.cs
+++ MizyBureau/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MizyBureau.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,17 @@ namespace MizyBureau.Properties {
         public string MizyDBConnectionString {
             get {
                 return ((string)(this["MizyDBConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\MizyLocalDB.m" +
+            "df;Integrated Security=True")]
+        public string MizyLocalDBConnectionString {
+            get {
+                return ((string)(this["MizyLocalDBConnectionString"]));
             }
         }
     }
